255b76a9df8226337b698477d71cb7595694a5fc
Sezgin Serin <sezgin.serin@hasel.com>
2022-10-11 06:49:26 +0300

    any Route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Product CRUD application built via Laravel 9 + Vue.js</title>
+    <title>CRUD</title>
 
 </head>
 

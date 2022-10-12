cec299e700e5c8b2fd754d63735914478deb8b40
Sezgin Serin <sezgin.serin@hasel.com>
2022-10-12 20:52:07 +0300

    Storage Delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -107,6 +108,8 @@ class ProductController extends Controller
     public function destroy($id)
     {
         $product = Product::find($id);
+
+        Storage::disk('public')->delete($product->picture);
 
         $product->delete();
 
